4c38b0f25117f26551f6ec95ad11fab44b2c445b
Marco Gomes <marco@mgsantos.com.br>
2025-12-31 07:48:20 -0300

    Adds pythons app

---
+++ python-app/template/src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now(),
+        'hostname' : socket.gethostname(),
+        'message' :  'Esta tudo indosss dbem humancodoidesssss!asa!',
+        'env': '${{values.app_env}}',
+        'App': '${{values.app_name}}'
+    })
+
+
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'UP'
+    }), 200
+
+
+if __name__ == '__main__':
+    # O parâmetro host='0.0.0.0' é a chave aqui
+    app.run(host='0.0.0.0', port=5000)
